d2c9c9e6076c8e05c13fa328a137f6c51cc9a886
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-11-17 20:13:28 -0600

    copia del repositorio
    de Youtube

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AppYoutube'`, () => {
+  it(`should have as title 'YoutubeApp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AppYoutube');
+    expect(app.title).toEqual('YoutubeApp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('AppYoutube app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('YoutubeApp app is running!');
   });
 });
